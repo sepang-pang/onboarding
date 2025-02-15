c0590712cf2e995a75477246fc073cd9ce916a0e
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-15 19:09:11 +0900

    [ feat ] UserRepository 구현 ( #3 )

---
+++ src/main/java/com/backend/onboarding/infrastructure/repository/JpaUserRepository.java
@@ -0,0 +1,12 @@
+package com.backend.onboarding.infrastructure.repository;
+
+import com.backend.onboarding.domain.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+}
---
+++ src/main/java/com/backend/onboarding/infrastructure/repository/UserRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.backend.onboarding.infrastructure.repository;
+
+import com.backend.onboarding.domain.model.UserEntity;
+import com.backend.onboarding.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final JpaUserRepository jpaUserRepository;
+
+    @Override
+    public Optional<UserEntity> findByUsername(String username) {
+        return jpaUserRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserEntity save(UserEntity userEntity) {
+        return jpaUserRepository.save(userEntity);
+    }
+
+}
