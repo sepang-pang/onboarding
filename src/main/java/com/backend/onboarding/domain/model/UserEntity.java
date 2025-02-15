71eaa84dc1bea914ca293234bb8def360aa6d964
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-15 17:16:44 +0900

    [ feat ] UserEntity 엔티티 추가 ( #3 )

---
+++ src/main/java/com/backend/onboarding/domain/model/UserEntity.java
@@ -0,0 +1,52 @@
+package com.backend.onboarding.domain.model;
+
+import com.backend.onboarding.domain.model.constraint.RoleType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private RoleType role;
+
+    @Builder
+    public UserEntity(String username, String password, String nickname, RoleType role) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public static UserEntity create(String username, String password, String nickname, RoleType role) {
+        return UserEntity.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .role(role)
+                .build();
+    }
+
+}
+
