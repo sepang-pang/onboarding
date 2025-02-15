2f1a165a390ff292f5d86a21216f7575ee1310ef
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-15 18:24:15 +0900

    [ feat ] 인증 관련 API 스웨거 문서 추가 ( #3 )

---
+++ src/main/java/com/backend/onboarding/infrastructure/docs/AuthControllerApiV1Swagger.java
@@ -0,0 +1,28 @@
+package com.backend.onboarding.infrastructure.docs;
+
+import com.backend.onboarding.application.res.ResAuthPostSignupDTOApiV1;
+import com.backend.onboarding.presentation.req.ReqAuthPostSignupDTOApiV1;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Auth", description = "회원가입, 로그인 인증 등 인증 관련 API를 제공합니다.")
+@RequestMapping("/v1/auth")
+public interface AuthControllerApiV1Swagger {
+
+    @Operation(summary = "회원가입", description = "회원가입을 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = ResAuthPostSignupDTOApiV1.class))),
+            @ApiResponse(responseCode = "400", description = "회원가입 실패.", content = @Content(schema = @Schema(implementation = ResAuthPostSignupDTOApiV1.class)))
+    })
+    @PostMapping("/signup")
+    ResAuthPostSignupDTOApiV1 signup(@RequestBody @Valid ReqAuthPostSignupDTOApiV1 dto);
+
+}
