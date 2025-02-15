4e4c80725fc185fa49ffb834b032b3d2e6da8d55
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-15 19:10:09 +0900

    [ feat ] 회원가입 기능 구현 ( #3 )
    - 아이디 중복 검증
    
    - 비밀번호 암호화 처리

--- src/main/java/com/backend/onboarding/presentation/controller/AuthControllerAPiV1.java
+++ src/main/java/com/backend/onboarding/presentation/controller/AuthControllerAPiV1.java
@@ -1,8 +1,7 @@
 package com.backend.onboarding.presentation.controller;
 
 import com.backend.onboarding.application.res.ResAuthPostSignupDTOApiV1;
-import com.backend.onboarding.domain.model.UserEntity;
-import com.backend.onboarding.domain.model.constraint.RoleType;
+import com.backend.onboarding.application.service.AuthServiceApiV1;
 import com.backend.onboarding.infrastructure.docs.AuthControllerApiV1Swagger;
 import com.backend.onboarding.presentation.req.ReqAuthPostSignupDTOApiV1;
 import jakarta.validation.Valid;
@@ -17,16 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/auth")
 public class AuthControllerAPiV1 implements AuthControllerApiV1Swagger {
 
+    private final AuthServiceApiV1 authServiceApiV1;
+
     @PostMapping("/signup")
     public ResAuthPostSignupDTOApiV1 signup(@RequestBody @Valid ReqAuthPostSignupDTOApiV1 dto) {
-
-        UserEntity dummy = UserEntity.create(
-                "dummy-user",
-                "dummy-password",
-                "dummy-nickname",
-                RoleType.USER
-        );
-
-        return ResAuthPostSignupDTOApiV1.of(dummy);
+        return authServiceApiV1.signup(dto);
     }
+
 }
