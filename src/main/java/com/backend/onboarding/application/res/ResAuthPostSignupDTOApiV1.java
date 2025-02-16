b040f19999a92ec9d21fcebf3824e1752ed98e42
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-16 16:51:02 +0900

    [ refactor ] 회원가입 응답 DTO에 권한 정보 추가 ( #7 )
    - ResAuthPostSignupDTOApiV1에 `authorities` 필드 추가
    
    - UserEntity의 Role 정보를 기반으로 권한 리스트 매핑
    
    - 내부 정적 클래스로 `Authority` 추가하여 역할 정보 관리

--- src/main/java/com/backend/onboarding/application/res/ResAuthPostSignupDTOApiV1.java
+++ src/main/java/com/backend/onboarding/application/res/ResAuthPostSignupDTOApiV1.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @Builder
 @NoArgsConstructor
@@ -14,15 +16,30 @@ public class ResAuthPostSignupDTOApiV1 {
 
     private String username;
     private String nickname;
-
-    // --
-    // TODO : 권한 추가 필요
-    // --
+    private List<Authority> authorities;
 
     public static ResAuthPostSignupDTOApiV1 of(UserEntity userEntity) {
         return ResAuthPostSignupDTOApiV1.builder()
                 .username(userEntity.getUsername())
                 .nickname(userEntity.getNickname())
+                .authorities(List.of(Authority
+                        .from(userEntity.getRole().getAuthority()))
+                )
                 .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Authority {
+
+        private String authorityName;
+
+        public static Authority from(String authorityName) {
+            return Authority.builder()
+                    .authorityName(authorityName)
+                    .build();
+        }
     }
 }
