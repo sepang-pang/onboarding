3326dd4af29136dde6d84a824872bc8bdcd84cf7
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-16 16:59:39 +0900

    [ feat ] 공통 응답 DTO 추가 ( #7 )

---
+++ src/main/java/com/backend/onboarding/application/global/dto/ResDTO.java
@@ -0,0 +1,16 @@
+package com.backend.onboarding.application.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResDTO<T> {
+    private Integer code;
+    private String message;
+    private T data;
+}
