9141cada42c07f3b87e6ec4ddfcc52df722a5a34
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-16 10:01:08 +0900

    [ feat ] 로그인 기능 구현 및 Refresh Token Redis 저장 ( #5 )
    - 로그인 요청을 처리하는 sign API 엔드포인트 추가
    
    - 사용자 검증 후 JWT Access Token 및 Refresh Token 생성

--- src/main/java/com/backend/onboarding/application/service/AuthServiceApiV1.java
+++ src/main/java/com/backend/onboarding/application/service/AuthServiceApiV1.java
@@ -1,11 +1,16 @@
 package com.backend.onboarding.application.service;
 
+import com.backend.onboarding.application.res.ResAuthPostLoginDTOApiV1;
 import com.backend.onboarding.application.res.ResAuthPostSignupDTOApiV1;
 import com.backend.onboarding.domain.model.UserEntity;
 import com.backend.onboarding.domain.model.constraint.RoleType;
 import com.backend.onboarding.domain.repository.UserRepository;
+import com.backend.onboarding.infrastructure.utl.JwtUtil;
+import com.backend.onboarding.presentation.req.ReqAuthPostLoginDTOApiV1;
 import com.backend.onboarding.presentation.req.ReqAuthPostSignupDTOApiV1;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +21,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthServiceApiV1 {
 
-    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RedisRefreshTokenService redisRefreshTokenService;
 
     @Transactional
     public ResAuthPostSignupDTOApiV1 signup(ReqAuthPostSignupDTOApiV1 dto) {
@@ -36,10 +43,36 @@ public class AuthServiceApiV1 {
         );
     }
 
+    @Transactional
+    public ResAuthPostLoginDTOApiV1 sign(ReqAuthPostLoginDTOApiV1 dto, HttpServletResponse response) {
+
+        UserEntity userEntity = userRepository.findByUsernameAndDeletedAtIsNull(dto.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("아이디를 정확히 입력해주세요."));
+
+        validatePasswordMatches(dto, userEntity);
+
+        String accessJwt = jwtUtil.generateAccessJwt(userEntity.getUsername(), userEntity.getRole());
+        String refreshJwt = jwtUtil.generateRefreshJwt();
+
+        // Redis 에 Refresh 토큰 저장
+        redisRefreshTokenService.saveRefreshToken(userEntity.getUsername(), refreshJwt);
+
+        // 리프레시 토큰 쿠키에 담기
+        response.addCookie(jwtUtil.generateRefreshJwtCookie(refreshJwt));
+
+        return ResAuthPostLoginDTOApiV1.of(accessJwt);
+    }
+
     private void checkUsernameDuplication(String username) {
         Optional<UserEntity> userEntityOptional = userRepository.findByUsername(username);
         if (userEntityOptional.isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+    }
+
+    private void validatePasswordMatches(ReqAuthPostLoginDTOApiV1 dto, UserEntity userEntity) {
+        if (!passwordEncoder.matches(dto.getPassword(), userEntity.getPassword())) {
+            throw new IllegalArgumentException("비밀번호를 정확히 입력해주세요.");
         }
     }
 }
--- src/main/java/com/backend/onboarding/infrastructure/docs/AuthControllerApiV1Swagger.java
+++ src/main/java/com/backend/onboarding/infrastructure/docs/AuthControllerApiV1Swagger.java
@@ -1,6 +1,8 @@
 package com.backend.onboarding.infrastructure.docs;
 
+import com.backend.onboarding.application.res.ResAuthPostLoginDTOApiV1;
 import com.backend.onboarding.application.res.ResAuthPostSignupDTOApiV1;
+import com.backend.onboarding.presentation.req.ReqAuthPostLoginDTOApiV1;
 import com.backend.onboarding.presentation.req.ReqAuthPostSignupDTOApiV1;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,5 +28,14 @@ public interface AuthControllerApiV1Swagger {
     })
     @PostMapping("/signup")
     ResponseEntity<ResAuthPostSignupDTOApiV1> signup(@RequestBody @Valid ReqAuthPostSignupDTOApiV1 dto);
+
+
+    @Operation(summary = "로그인", description = "로그인을 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = ResAuthPostSignupDTOApiV1.class))),
+            @ApiResponse(responseCode = "400", description = "회원가입 실패.", content = @Content(schema = @Schema(implementation = ResAuthPostSignupDTOApiV1.class)))
+    })
+    @PostMapping("/sign")
+    ResponseEntity<ResAuthPostLoginDTOApiV1> sign(@RequestBody @Valid ReqAuthPostLoginDTOApiV1 dto, HttpServletResponse response);
 
 }
