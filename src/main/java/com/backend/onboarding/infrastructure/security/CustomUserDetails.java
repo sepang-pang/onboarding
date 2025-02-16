ea1e09849c4cea46befdf48fbfae1679d56596ba
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-02-16 16:35:07 +0900

    [ refactor ] UserEntity 직접 노출 방지를 위한 CustomUserDetails 리팩토링 ( #5 )
    - UserEntity를 직접 저장하지 않고, 내부 정적 클래스 User를 통해 필요한 정보만 저장하도록 변경
    
    - User.from(userEntity) 메서드를 추가하여 변환 로직을 캡슐화
    
    - UserEntity 대신 User 클래스를 사용하여 정보 접근

--- src/main/java/com/backend/onboarding/infrastructure/security/CustomUserDetails.java
+++ src/main/java/com/backend/onboarding/infrastructure/security/CustomUserDetails.java
@@ -5,6 +5,7 @@ import com.backend.onboarding.domain.model.constraint.RoleType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,27 +18,48 @@ import java.util.Collection;
 @AllArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
-    private final UserEntity userEntity;
+    private User user;
 
     public static CustomUserDetails of(UserEntity userEntity) {
         return CustomUserDetails.builder()
-                .userEntity(userEntity)
+                .user(User.from(userEntity))
                 .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class User {
+
+        private Long id;
+        private String username;
+        private String password;
+        private RoleType role;
+
+        public static User from(UserEntity userEntity) {
+            return User.builder()
+                    .id(userEntity.getId())
+                    .username(userEntity.getUsername())
+                    .password(userEntity.getPassword())
+                    .role(userEntity.getRole())
+                    .build();
+        }
     }
 
     @Override
     public String getPassword() {
-        return userEntity.getPassword();
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return userEntity.getUsername();
+        return user.getUsername();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        RoleType role = userEntity.getRole();
+        RoleType role = user.getRole();
         String authority = role.getAuthority();
 
         SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
